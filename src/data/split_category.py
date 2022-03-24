37ae9597b7fbf063180939cd6da006ba1d5d154c
qiushi <qiushi.yann@gmail.com>
2022-03-23 22:47:38 -0500

    simplify splitting

--- src/data/split_category.py
+++ src/data/split_category.py
@@ -13,10 +13,10 @@ def read_sheet(sheet: int):
     return dk.delayed(pd.read_excel)(raw_data_dir / raw_data_name, sheet_name=sheet)
 
 
-def extract_category(ddf_all, category: str, by: str):
+def extract_category(ddf_factors, ddf_categories, category: str, by: str):
     p = re.compile(r" |&|/")
-    ddf = ddf_all.loc[ddf_all[by] == category, ["Date", "Variable", "value"]]
-    ddf = ddf.pivot(index="Date", columns="Variable", values="value")
+    factors = ddf_categories.loc[ddf_categories[by] == category, "Variable"]
+    ddf = ddf_factors[factors]
     category = p.sub("-", category, 1).replace("& ", "").replace(" ", "-").lower()
     return ddf.to_parquet(
         processed_data_dir / "split" / by.lower() / f"{category}.parquet"
@@ -25,15 +25,16 @@ def extract_category(ddf_all, category: str, by: str):
 
 def split_df(by: Union[Literal["Category"], Literal["Subcategory"]] = "Subcategory"):
     if by not in ["Category", "Subcategory"]:
-        print("Invalid split option, must be either 'Category' or 'Subcategory'")
+        raise Exception(
+            "Invalid split option, must be either 'Category' or 'Subcategory'"
+        )
     ddf_factors = read_sheet(1)
     ddf_categories = read_sheet(2)
-    ddf_factors_long = ddf_factors.melt(
-        id_vars=["Date"], var_name="Variable", value_name="value"
-    )
-    ddf_all = ddf_factors_long.merge(ddf_categories, on="Variable", how="left")
     categories = ddf_categories[by].unique().compute()
-    ddfs = [extract_category(ddf_all, category, by) for category in categories]
+    ddfs = [
+        extract_category(ddf_factors, ddf_categories, category, by)
+        for category in categories
+    ]
 
     dk.compute(*ddfs)
     return True
